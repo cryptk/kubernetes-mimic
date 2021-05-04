cab6aa3df723f6e09fc7a539350f2440598bebfc
Chris Jowett <cryptk@gmail.com>
2021-05-04 09:06:44 -0500

    Start adding some tests

---
+++ internal/utils/utils_test.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBoolAddr(t *testing.T) {
+	type args struct {
+		b bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want *bool
+	}{
+		{"Test positive bool", args{b: true}, func() *bool { b := true; return &b }()},
+		{"Test negative bool", args{b: false}, func() *bool { b := false; return &b }()},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BoolAddr(tt.args.b); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BoolAddr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
